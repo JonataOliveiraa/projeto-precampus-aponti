af436601c7479e6d6dcd0649ba0811d5a9d0fbd6
JonataOliveiraa <122224076+JonataOliveiraa@users.noreply.github.com>
2026-01-08 09:27:53 -0300

    add-label-text-and-selected-list-with-full-object-option

--- src/layout/components/FormMultiSelect.tsx
+++ src/layout/components/FormMultiSelect.tsx
@@ -1,29 +1,22 @@
 import { useEffect, useState } from "react";
 
-const OPTIONS = [
-    { label: "Universidade Federal de São Paulo", value: "ufsp" },
-    { label: "Universidade Estadual do Rio de Janeiro", value: "uerj" },
-    { label: "Universidade Católica de Minas Gerais", value: "ucmg" },
-    { label: "Universidade Privada de Brasília", value: "upb" },
-    { label: "Universidade Federal do Nordeste", value: "ufn" }
-];
+interface option {
+    label: string,
+    value: string
+}
 
-function FormMultiSelect({ label, className }: { label: string, className?: string }) {
+function FormMultiSelect({ label, options, className }: { label: string, options: option[],  className?: string }) {
     const [isOpen, setIsOpen] = useState(false);
-    const [selected, setSelected] = useState<string[]>([]) // Guardamos apenas os 'values' (strings)
+    const [selected, setSelected] = useState<option[]>([]) // Guardamos apenas os 'values' (strings)
 
-    useEffect(() => {
-        console.log("O usuário mudou a seleção para:", selected);
-    }, [selected])
-
-    const handleSelect = (value: string) => {
-        if (selected.includes(value)) {
-            setSelected(selected.filter(item => item !== value));
+    const handleSelect = (option: option) => {
+        if (selected.includes(option)) {
+            setSelected(selected.filter(item => item !== option));
         } else if (selected.length < 3) {
-            setSelected([...selected, value]);
+            setSelected([...selected, option]);
         } else {
             selected.shift()
-            setSelected([...selected, value])
+            setSelected([...selected, option])
         }
     }
 
@@ -36,7 +29,7 @@ function FormMultiSelect({ label, className }: { label: string, className?: stri
                 onClick={() => setIsOpen(true)}
             >
                 {selected.length > 0 
-                    ? `${selected.length} selecionado(s)` 
+                    ? `${selected.map(e => e.label).join(', ')} selecionado(s)` 
                     : "Abrir tela"}
             </button>
 
@@ -46,32 +39,25 @@ function FormMultiSelect({ label, className }: { label: string, className?: stri
                         <p className="font-semibold">Selecione até 3 cursos:</p>
                         
                         <div className="flex flex-col bg-zinc-200 p-3 mb-3 rounded-md max-h-60 overflow-auto gap-3">
-                            {OPTIONS.map(element => {
-                                // 3. Verificamos se está selecionado para aplicar o estilo dinamicamente
-                                const isSelected = selected.includes(element.value);
+                            {options.map(element => {
+                                const isSelected = selected.find(op => op.value === element.value)
 
                                 return (
                                     <div 
                                         key={element.value}
-                                        onClick={() => handleSelect(element.value)}
+                                        onClick={() => handleSelect(element)}
                                         className={`p-2 rounded-md cursor-pointer transition-colors ${
                                             isSelected 
                                                 ? 'bg-blue-500 text-white hover:bg-blue-600' 
                                                 : 'bg-white text-black hover:bg-blue-100'
                                         }`}
                                     >
-                                        Curso: {element.label}
+                                        {element.value.toUpperCase()}: {element.label}
                                     </div>
                                 )
                             })}
                         </div>
-                        <div className="border px-2">
-                            {
-                                selected.map(element => (
-                                    <div className="bg-blue-700 text-white">{OPTIONS.find(value => element === value.value)}</div>
-                                ))
-                            }
-                        </div>
+                        
                         <button
                             onClick={() => setIsOpen(false)}
                             className="mt-4 w-full py-2 bg-gray-100 rounded-lg font-medium hover:bg-gray-200"
