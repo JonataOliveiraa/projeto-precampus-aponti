5db3e246a786a5d55024b28f741d99c2e62a6fd8
JonataOliveiraa <122224076+JonataOliveiraa@users.noreply.github.com>
2026-01-13 10:24:13 -0300

    fix-forms-font

--- src/layout/components/FormSubtitle.tsx
+++ src/layout/components/FormSubtitle.tsx
@@ -1,5 +1,5 @@
 function FormSubtitle({ text }: { text: string }) {
-    return <h2 className="text-2xl font-semibold mb-4">{text}</h2>;
+    return <h2 className="font-heading text-2xl font-semibold mb-4">{text}</h2>;
 }
 
 export default FormSubtitle;
