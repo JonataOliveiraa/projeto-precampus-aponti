45336b129d61c641fe64384dac92976aa87f2898
Adriana <41.adriana.silva@gmail.com>
2026-01-20 10:40:35 -0300

    setup

--- src/pages/components/ResetPasswordMessage.tsx
+++ src/pages/components/ResetPasswordMessage.tsx
@@ -1,20 +1,22 @@
 import type React from "react";
-
+import { RiCloseCircleLine, RiCloseFill } from "@remixicon/react";
 function ResetPasswordMessage({ Close }: { Close: (e: React.MouseEvent<HTMLDivElement, MouseEvent> ) => void }) {
 
   return (
     <div className="fixed min-h-screen min-w-screen bg-white/50 z-20 flex items-center justify-center">
-      <div className="relative bg-white p-2 border border-red-500 py-20 rounded-2xl w-95">
-        <div
-          className="fixed   top-90 right-197 cursor-pointer"
-          onClick={Close}
-        >
-          <span className="text-orange-700 text-3xl font-bold">X</span>
+      <div className="relative bg-white border border-blue-500 rounded-2xl w-95 overflow-hidden">
+        <div className="flex justify-end"> 
+          <span className="px-2 py-1">
+            <RiCloseCircleLine className="text-blue-900"/>
+          </span>
         </div>
-        <h1 className="text-xl text-justify font-semibold">
-          Para redefinir sua senha, clique no link que foi enviado para seu
-          email cadastrado!
-        </h1>
+        <div className="px-5 pb-4">
+          <h1 className="text-xl font-bold mb-7"> Email enviado</h1>
+          <div className="ml-2 bg-blue-100 p-2 rounded-md">
+            <p> O email foi enviado com sucesso.    </p>
+            <p>  Verifique sua caixa de entrada.</p>
+          </div>
+        </div>
       </div>
     </div>
   );
