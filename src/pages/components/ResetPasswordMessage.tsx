c8206f726e88757fed68baf3508f4f93927a0c5f
Adriana <41.adriana.silva@gmail.com>
2026-01-22 09:33:38 -0300

    button-added

--- src/pages/components/ResetPasswordMessage.tsx
+++ src/pages/components/ResetPasswordMessage.tsx
@@ -1,5 +1,7 @@
 import type React from "react";
 import { RiCloseCircleLine, RiCloseFill } from "@remixicon/react";
+import Button from "../../components/Button";
+import { Link } from "react-router-dom";
 function ResetPasswordMessage({ Close }: { Close: (e: React.MouseEvent<HTMLDivElement, MouseEvent> ) => void }) {
 
   return (
@@ -7,15 +9,17 @@ function ResetPasswordMessage({ Close }: { Close: (e: React.MouseEvent<HTMLDivEl
       <div className="relative bg-white border border-blue-500 rounded-2xl w-95 overflow-hidden">
         <div className="flex justify-end"> 
           <span className="px-2 py-1">
-            <RiCloseCircleLine className="text-blue-900"/>
           </span>
         </div>
         <div className="px-5 pb-4">
           <h1 className="text-xl font-bold mb-7"> Email enviado</h1>
-          <div className="ml-2 bg-blue-100 p-2 rounded-md">
+          <div className="ml-2 bg-blue-100 p-2 rounded-md mb-5">
             <p> O email foi enviado com sucesso.    </p>
             <p>  Verifique sua caixa de entrada.</p>
           </div>
+          <Link to="/login">
+            <Button text="Ok"/>
+          </Link>
         </div>
       </div>
     </div>
